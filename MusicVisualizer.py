7b6c74dc0d28ad4425850a5339bebd2c84dc9ff6
Nathanael Acker <35240113+TheRealPiGuy@users.noreply.github.com>
2021-01-22 18:55:23 -0600

    Update MusicVisualizer.py

--- MusicVisualizer.py
+++ MusicVisualizer.py
@@ -2,13 +2,12 @@ import librosa
 import numpy as np
 import pygame
 import math
-import tkinter as tk
+from tkinter import *
 from tkinter import filedialog
 from tkinter import colorchooser
-
 import os
 
-playing = True
+playing = False # Global variable, represents if music is playing or not
 
 
 def clamp(min_value, max_value, value):
@@ -31,7 +30,6 @@ class AudioBar:
         self.min_decibel, self.max_decibel = min_decibel, max_decibel
         self.__decibel_height_ratio = (self.max_height - self.min_height)/(self.max_decibel - self.min_decibel)
 
-    #modified to include color update
     def update(self, dt, decibel,color):
         self.color = color
         desired_height = decibel * self.__decibel_height_ratio + self.max_height
@@ -43,18 +41,19 @@ class AudioBar:
         pygame.draw.rect(screen, self.color, (self.x, self.y + self.max_height - self.height, self.width, self.height))
 
 
-filename = "Alan Walker - Force [NCS Release].wav"
-filePath = filedialog.askopenfilename(title="Select a Music File",filetypes=[(".WAV Files",'*.wav')])
+filename = "ring.wav"
+#filePath = filedialog.askopenfilename(title="Select a Music File",filetypes=[(".WAV Files",'*.wav')])
+
 
 
 #color wheel
-bar_color = (71, 159, 161)
+bar_color = (71,159,161)
 colorWheelRadius = 50
 
-def color() :
+def color():
     try:
         new_color = colorchooser.askcolor()[1]
-        print("" + new_color) #seems to fix crash
+        print("" + new_color)
         return new_color
     except Exception:
         return bar_color
@@ -71,12 +70,12 @@ def get_background_color(background_color):
     black = (0,0,0)
     white = (255,255,255)
     if background_color == black: return white
-    elif background_color == white: return red
-    elif background_color == red: return orange
-    elif background_color == orange: return yellow 
-    elif background_color == yellow: return green 
-    elif background_color == green: return blue 
-    elif background_color == indigo: return violet
+    #elif background_color == white: return red
+    #elif background_color == red: return orange
+    #elif background_color == orange: return yellow 
+    #elif background_color == yellow: return green 
+    #elif background_color == green: return blue 
+    #elif background_color == indigo: return violet
     else: return black
 
 def get_text_color(background_color):
@@ -86,8 +85,7 @@ background_color = (0,0,0)
 text_color = get_background_color(background_color)
 
 
-
-time_series, sample_rate = librosa.load(filePath)  # getting information from the file
+time_series, sample_rate = librosa.load(filename)  # getting information from the file
 
 # getting a matrix which contains amplitude values according to frequency and time indexes
 stft = np.abs(librosa.stft(time_series, hop_length=512, n_fft=2048*4))
@@ -113,6 +111,8 @@ pygame.display.set_icon(icon)
 
 
 
+
+
 infoObject = pygame.display.Info()
 
 
@@ -126,7 +126,6 @@ screen_h = int(infoObject.current_w/2.5)
 
 # Set up the drawing window
 screen = pygame.display.set_mode([screen_w, screen_h])
-
 
 
 bars = []
@@ -149,26 +148,26 @@ for c in frequencies:
 t = pygame.time.get_ticks()
 getTicksLastFrame = t
 
-pygame.mixer.music.load(filePath)
-pygame.mixer.music.play(0)
-pygame.mixer.music.set_volume(0.05)
+#pygame.mixer.music.load(filePath)
+#pygame.mixer.music.play(0)
+#pygame.mixer.music.set_volume(0.05)
 
 
 myFont = pygame.font.SysFont('Courier New',20)
-# text = myFont.render('Now Playing: ' + os.path.basename(filePath),True,text_color) #moved in while loop
+text = myFont.render('Now Playing: ',True,(255,255,255))
 
 playPauseImage = pygame.image.load('playpause2.png')
 folderImage = pygame.image.load('folder2.png')
 
 
-
 songName = ''
+
 # Run until the user asks to quit
 running = True
 while running:
 
-    text = myFont.render('Now Playing: ' + os.path.basename(filePath),True,text_color)
-    
+    text = myFont.render('Now Playing: ',True,text_color)
+
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
     
@@ -187,7 +186,6 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
-            #check if pause/play was clicked
             if math.sqrt(sqx + sqy) < 75:
                 if(playing):
                     playing = False
@@ -197,54 +195,62 @@ while running:
                     playing = True
                     pygame.mixer.music.unpause()
                     print('on')
-            #check if folder was clicked
             elif math.sqrt(sqx2+sqy2) < 60:
+                root = Tk()
                 pygame.mixer.music.stop()
                 item = filedialog.askopenfilename(title="Select a Music File",filetypes=[(".WAV Files",'*.wav')])
+                
+                root.destroy()
                 if(item != ''):
-                    songName = os.path.basename(item)
                     time_series, sample_rate = librosa.load(item)  # getting information from the file
-
+                    
+                    
+                    
                     # getting a matrix which contains amplitude values according to frequency and time indexes
                     stft = np.abs(librosa.stft(time_series, hop_length=512, n_fft=2048*4))
                     spectrogram = librosa.amplitude_to_db(stft, ref=np.max)  # converting the matrix to decibel matrix
                     frequencies = librosa.core.fft_frequencies(n_fft=2048*4)  # getting an array of frequencies
+                    times = librosa.core.frames_to_time(np.arange(spectrogram.shape[1]), sr=sample_rate, hop_length=512, n_fft=2048*4)
+                    time_index_ratio = len(times)/times[len(times) - 1]
+                    frequencies_index_ratio = len(frequencies)/frequencies[len(frequencies)-1]
+
+                    
+                    
                     pygame.mixer.music.load(item)
                     pygame.mixer.music.play()
+                    
                 
-                text = myFont.render('Now Playing: ' + songName,True,(255,255,255))
-            #check if color wheel was clicked
+                text = myFont.render('Now Playing: ' + os.path.basename(item),True,(255,255,255))
             elif math.sqrt(sqx3 + sqy3) < colorWheelRadius:
                 try:
                     bar_color = color()
                 except Exception:
                     print(Exception)
-            #if nothing else is clicked, toggle through background colors
             else:
                 background_color = get_background_color(background_color)
                 text_color = get_text_color(background_color)
                 
 
-    # Fill the background
+
+    # Fill the background with black
+    #screen.fill((0, 0, 0))
     screen.fill(background_color)
 
-    #folder icon stuff
-    pygame.draw.circle(screen,(0,0,0),[420,150],50)  
+    
+
+    pygame.draw.circle(screen,(0,0,0),[420,150],50) 
     sqx2 = (x - 420)**2
     sqy2 = (y - 150)**2
     
-    #render things
     screen.blit(playPauseImage,(150,50))
     screen.blit(folderImage,(360,100))
     screen.blit(text,(20,270))
 
-    #render color wheel
     sqx3 = (x - 90)**2
     sqy3 = (y - 150)**2
     colorWheel = pygame.image.load('colorWheel.png')
     screen.blit(colorWheel,(15,97))
-
-    #update bars throughout song
+    
     for b in bars:
         b.update(deltaTime, get_decibel(pygame.mixer.music.get_pos()/1000.0, b.freq),bar_color)
         b.render(screen)
